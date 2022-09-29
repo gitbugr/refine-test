ec9bf5a8cfd081e3ad7f97dbb099c0ff8f9d34f0
Kyron Taylor <kyron@linkmaker.co.uk>
2022-09-29 22:10:50 +0100

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { Refine } from "@pankod/refine-core";
+import {
+  notificationProvider,
+  ReadyPage,
+  ErrorComponent,
+  LoginPage,
+} from "@pankod/refine-antd";
+import routerProvider from "@pankod/refine-react-router-v6";
+import { dataProvider } from "@pankod/refine-supabase";
+import { RefineKbarProvider } from "@pankod/refine-kbar";
+import { useTranslation } from "react-i18next";
+import "styles/antd.less";
+import { supabaseClient } from "utility";
+import {
+  Title,
+  Header,
+  Sider,
+  Footer,
+  Layout,
+  OffLayoutArea,
+} from "components/layout";
+import authProvider from "./authProvider";
+
+function App() {
+  const { t, i18n } = useTranslation();
+
+  const i18nProvider = {
+    translate: (key: string, params: object) => t(key, params),
+    changeLocale: (lang: string) => i18n.changeLanguage(lang),
+    getLocale: () => i18n.language,
+  };
+
+  return (
+    <RefineKbarProvider>
+      <Refine
+        notificationProvider={notificationProvider}
+        ReadyPage={ReadyPage}
+        catchAll={<ErrorComponent />}
+        routerProvider={routerProvider}
+        dataProvider={dataProvider(supabaseClient)}
+        authProvider={authProvider}
+        LoginPage={LoginPage}
+        Title={Title}
+        Header={Header}
+        Sider={Sider}
+        Footer={Footer}
+        Layout={Layout}
+        OffLayoutArea={OffLayoutArea}
+        i18nProvider={i18nProvider}
+      />
+    </RefineKbarProvider>
+  );
+}
+
+export default App;
